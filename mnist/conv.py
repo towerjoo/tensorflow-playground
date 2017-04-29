e7d407517e9e6d5db92ab61824b45ef357a5265c
Zhu Tao <zhutao.iscas@gmail.com>
2017-04-29 19:00:23 +0800

    simple Conv model for mnist

---
+++ mnist/conv.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+class SimpleModel:
+    def __init__(self):
+        mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        self.x_train, self.y_train, self.x_test, self.y_test = \
+            mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+        self.x_train = self.x_train.reshape(-1, 28, 28, 1)
+        self.x_test = self.x_test.reshape(-1, 28, 28, 1)
+
+        self.X = tf.placeholder("float", [None, 28, 28, 1])
+        self.Y = tf.placeholder("float", [None, 10])
+        filter1 = self.init_weights([3, 3, 1, 32])
+        fc1 = self.init_weights([14*14*32, 635])
+        fc2 = self.init_weights([635, 10])
+        self.p_keep_conv = tf.placeholder("float")
+        self.p_keep_hidden = tf.placeholder("float")
+        self.train_model = self.get_model(self.X, filter1, fc1, fc2, self.p_keep_conv, self.p_keep_hidden)
+
+    def get_model(self, X, filter1, fc1, fc2, p_keep_conv, p_keep_hidden):
+        layer1a = tf.nn.relu(tf.nn.conv2d(X, filter1, strides=[1, 1, 1, 1], padding='SAME'))
+        layer1 = tf.nn.max_pool(layer1a, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                               padding='SAME')
+        layer1 = tf.nn.dropout(layer1, p_keep_conv)
+        layer1 = tf.reshape(layer1, [-1, fc1.get_shape().as_list()[0]])
+
+        x_fc = tf.nn.relu(tf.matmul(layer1, fc1))
+        x_fc = tf.nn.dropout(x_fc, p_keep_hidden)
+
+        out = tf.matmul(x_fc, fc2)
+        return out
+
+    def init_weights(self, shape):
+        return tf.Variable(tf.random_normal(shape, stddev=.01))
+
+    def get_cost(self):
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.train_model, labels=self.Y))
+
+    def train(self, batch_size=128, epoch=5, test_size=256):
+        train_op = tf.train.RMSPropOptimizer(.001, .9).minimize(self.get_cost())
+        predict_op = tf.argmax(self.train_model, 1)
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            for i in range(epoch):
+                train_batch = zip(range(0, len(self.x_train), batch_size),
+                                  range(batch_size, len(self.x_train)+1, batch_size))
+                for start, end in train_batch:
+                    sess.run(train_op, feed_dict={self.X: self.x_train[start:end], 
+                                        self.Y: self.y_train[start:end],
+                                        self.p_keep_conv: .8,
+                                        self.p_keep_hidden: .5})
+                test_index = np.arange(len(self.x_test))
+                np.random.shuffle(test_index)
+                test_index = test_index[:test_size]
+                print i, np.mean(np.argmax(self.y_test[test_index], axis=1) ==
+                            sess.run(predict_op, feed_dict={self.X: self.x_test[test_index],
+                                        self.p_keep_conv: 1.,
+                                        self.p_keep_hidden: 1.}))
+
+
+        
+if __name__ == "__main__":
+    model = SimpleModel()
+    model.train()
